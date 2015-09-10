4eede4d4bfcc09f54bfcd9c7185e3d62f4c27d70
sergbek <sergbek@ukr.net>
2015-09-10 22:47:21 +0300

    commit

---
+++ app/src/main/java/com/example/sergbek/pushnotifications/MyGcmListenerService.java
@@ -0,0 +1,8 @@
+package com.example.sergbek.pushnotifications;
+
+import com.google.android.gms.gcm.GcmListenerService;
+
+
+public class MyGcmListenerService extends GcmListenerService {
+
+}
---
+++ app/src/main/java/com/example/sergbek/pushnotifications/MyInstanceIDListenerService.java
@@ -0,0 +1,9 @@
+package com.example.sergbek.pushnotifications;
+
+import com.google.android.gms.iid.InstanceIDListenerService;
+
+/**
+ * Created by Sergbek on 10.09.2015.
+ */
+public class MyInstanceIDListenerService extends InstanceIDListenerService {
+}
